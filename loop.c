604a83dac6820225e1547095f13267b5befb8e2d
Prithvi <prithviroy91@gmail.com>
2020-01-11 20:42:50 +0530

    loop test

---
+++ loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// compile: gcc <filename> -o <output_filename>
+// run the file : ./<output_filename>
+
+int main(){
+	int i=0;
+
+	for(i=1; i<=10; i++){
+		printf("\n%d", i);
+	}
+	printf("\n");
+}
