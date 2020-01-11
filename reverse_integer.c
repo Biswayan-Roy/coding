a02f59f080478760bc97206745c058d91bbefeab
Prithvi <prithviroy91@gmail.com>
2020-01-11 22:00:41 +0530

    leetcode 07

---
+++ reverse_integer.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+
+int reverse(int);
+
+int reverse(int x){
+	// write code here
+	int out=0,r;
+
+	while(x!=0){
+		r = x % 10;
+		out = out*10 + r;
+		x = x /10;
+	}
+
+	return out;
+
+}
+
+int main(){
+	int out;
+	int in =120;
+	out = reverse(in);
+	printf("\n%d\n", out);
+	return 0;
+}
